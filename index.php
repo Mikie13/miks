9f06c76def746aac8c2cee0fbcf4b35629853b56
Mikie13 <mikiesaludar6@gmail.com>
2024-11-11 22:09:11 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first Ubuntu Server PHP Deployment</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body>
+
+	<p>Ubuntu Server PHP Deployment</p>
+
+		<!-- Form starts here -->
+		<form method="POST" action="your-server-side-script.php">
+</body>
+<div>
+	<!-- Name Field -->
+	<div class="mb-3">
+	<label for="Firstname" class="form-label">First Name:</label>
+	<input type="text" name="Firstname"id="firstname" class="form-control" required>
+</div>
+
+<div>
+	<!-- Middle Name Field -->
+	<div class="mb-3">
+	<label for="Middlename" class="form-label">Middle Name:</label>
+	<input type="text" name="Middlename"id="middlename" class="form-control" required>
+</div>
+
+
+<div>
+	<!-- Last Name Field -->
+	<div class="mb-3">
+	<label for="Lastname" class="form-label">Last Name:</label>
+	<input type="text" name="Lastname"id="Lastname" class="form-control" required>
+</div>
+
+<div>
+	<!-- Age Field -->
+	<div class="mb-3">
+	<label for="Age" class="form-label">Age:</label>
+	<input type="text" name="Age"id="age" class="form-control" required>
+</div>
+
+<div>
+<!-- Address Field -->
+	<div class="mb-3">
+	<label for="Address" class="form-label">Address:</label>
+	<input type="text" name="Address"id="address" class="form-control" required>
+</div>
+
+<div>
+<!-- Course and Section Field -->
+	<div class="mb-3">
+	<label for="courseandsection" class="form-label">Course and Section:</label>
+	<input type="text" name="courseandsection"id="courseandsection" class="form-control" required>
+</div>
+
+
+<!-- Submit Button -->
+<div class="mb-3">
+	<button type="submit" class="btn btn-primary">Submit</button>
+</div>
+
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
